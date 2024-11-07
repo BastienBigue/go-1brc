929ff2c71a1f7bdc36a686cd55bb395be9ad67ac
Bastien Bigué <bastien_bigue@gmail.com>
2024-11-07 16:04:32 +0100

    Add simple result check at end of processing

--- main.go
+++ main.go
@@ -10,31 +10,39 @@ import (
 var NUMBER_OF_READERS = 8
 var READ_BUFFER_SIZE = 8 * 1024 * 1024
 
-// var TEST_FILE = "temperatures.csv"
-var TEST_FILE = "measurements.txt"
+// var TEST_FILE = IntputFile{"temperatures.csv", 10}
 
-// var TEST_FILE = "measurements_10M.txt"
+// var TEST_FILE = IntputFile{"draft.txt", 10}
+// var TEST_FILE = IntputFile{"measurements_100.txt", 100}
+// var TEST_FILE = IntputFile{"measurements_10k.txt", 10 * 1000}
+// var TEST_FILE = IntputFile{"measurements_1M.txt", 1000 * 1000}
+// var TEST_FILE = IntputFile{"measurements_10M.txt", 10 * 1000 * 1000}
+// var TEST_FILE = InputFile{"measurements_100M.txt", 100* 1000 * 1000}
+var TEST_FILE = IntputFile{"measurements.txt", 1000 * 1000 * 1000}
 
-// var TEST_FILE = "measurements_100M.txt"
-
-// var TEST_FILE = "measurements_1M.txt"
+type IntputFile struct {
+	fileName string
+	nbLines  int64
+}
 
 func main() {
-	//var wg sync.WaitGroup
+
 	go func() {
 		fmt.Println(http.ListenAndServe("localhost:6060", nil))
 	}()
 
-	//slog.SetLogLoggerLevel(slog.LevelDebug.Level())
+	// slog.SetLogLoggerLevel(slog.LevelDebug.Level())
+
 	startTime := time.Now()
-	processFile(TEST_FILE)
+	resultMap := processFile(TEST_FILE)
 	endTime := time.Now()
 	fmt.Printf("Execution time is %v\n", endTime.Sub(startTime))
+	checkResult(TEST_FILE, resultMap)
 }
 
-func processFile(s string) {
+func processFile(inputFile IntputFile) map[string]*MinMaxAverage {
 
-	splitFileReader := SplitFileReaders{nbReaders: NUMBER_OF_READERS, fileName: s}
+	splitFileReader := NewSplitFileReader(inputFile.fileName, NUMBER_OF_READERS)
 	reducer := NewReducer()
 	mapsChan, _ := splitFileReader.processFileConcurrently()
 
@@ -44,4 +52,15 @@ func processFile(s string) {
 	}
 
 	fmt.Println(reducer.resultMap)
+	return reducer.resultMap
+}
+
+func checkResult(inputFile IntputFile, resultMap map[string]*MinMaxAverage) {
+	var nbLinesProcessed int64
+	for _, v := range resultMap {
+		nbLinesProcessed += int64(v.count)
+	}
+	if inputFile.nbLines != nbLinesProcessed {
+		panic(fmt.Sprintf("Number of lines processed is incorrect ! Expected: %v, Got: %v", inputFile.nbLines, nbLinesProcessed))
+	}
 }
